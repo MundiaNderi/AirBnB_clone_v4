f928aad78aa943090fe388bcc67010face6d4268
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-06-03 00:28:28 +0300

    Create 1-hbnb.js

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  // Store the selected amenities
+  var selectedAmenities = [];
+
+  // Listen for changes on each input checkbox tag
+  $('input[type="checkbox"]').change(function() {
+    var amenityId = $(this).data('id');
+    var amenityName = $(this).data('name');
+
+    // Check if the checkbox is checked
+    if ($(this).is(':checked')) {
+      // Add the Amenity ID to the selected amenities list
+      selectedAmenities.push(amenityId);
+    } else {
+      // Remove the Amenity ID from the selected amenities list
+      var index = selectedAmenities.indexOf(amenityId);
+      if (index > -1) {
+        selectedAmenities.splice(index, 1);
+      }
+    }
+
+    // Update the h4 tag inside the div Amenities with the list of Amenities checked
+    $('.amenities h4').text(selectedAmenities.join(', '));
+  });
+});
