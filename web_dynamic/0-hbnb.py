7492afc8a9932bd031851900b230c0b9439f3566
MundiaNderi <sarahngima77@gmail.com>
2023-06-02 22:03:58 +0300

    Task 1

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -6,6 +6,8 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
+import os
+import uuid
 from flask import Flask, render_template
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
@@ -37,7 +39,8 @@ def hbnb():
     return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
